1b59df2c801b824535ce713c2bf096709bba6749
BYWallace <bwallace90@gmail.com>
2014-07-12 18:42:04 -0400

    set up js model and routes for user rankings

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,9 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
    root 'welcome#index'
+   resources :users do
+    resources :rankings, only:[:index]
+    end
    resources :movies, only:[:index]
 
   # Example of regular route:
