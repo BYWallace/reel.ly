f6e28151c742de9a8a75a9c670831e16fb64d3cd
BYWallace <bwallace90@gmail.com>
2014-07-13 13:53:04 -0400

    removed router for now

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -3,9 +3,7 @@ $(document).ready(function() {
   window.currentUserRankings.fetch();
 
   window.movieCollection.fetch().then(function() {
-    var router = new Router();
-    Backbone.history.start();
-    // var movieComparisonView = new MovieComparisonView({collection: movieCollection});
-    // movieComparisonView.$el.appendTo(".col-md-10");
+    var movieComparisonView = new MovieComparisonView({collection: movieCollection});
+    movieComparisonView.$el.appendTo(".col-md-10");
   });
 });
--- app/assets/javascripts/routers/router.js
+++ app/assets/javascripts/routers/router.js
@@ -1,19 +1,19 @@
-var Router = Backbone.Router.extend({
-  routes: {
-    "*default" : "compareMovies"
-  },
+// var Router = Backbone.Router.extend({
+//   routes: {
+//     "*default" : "compareMovies"
+//   },
 
-  clearView: function() {
-    if (this.view) {
-      this.view.remove();
-      this.view = null;
-    }
-  },
+//   clearView: function() {
+//     if (this.view) {
+//       this.view.remove();
+//       this.view = null;
+//     }
+//   },
 
-  compareMovies: function() {
-    console.log("compareMovies is firing");
-    this.clearView();
-    this.view = new MovieComparisonView({collection: movieCollection });
-    this.view.$el.appendTo(".col-md-10");
-  }
-});
+//   compareMovies: function() {
+//     console.log("compareMovies is firing");
+//     this.clearView();
+//     this.view = new MovieComparisonView({collection: movieCollection });
+//     this.view.$el.appendTo(".col-md-10");
+//   }
+// });
