ec933c1aafdaa3c5ec7fd557719b9f76a9ddc8a1
BYWallace <bwallace90@gmail.com>
2014-07-15 11:52:29 -0400

    more randomization tweaks and sign out button placement

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,8 +1,9 @@
 class MoviesController < ApplicationController
   def index
     # postgreSQL
-    if rand(1..10) > 4 && current_user.movies.count > 4
-      @movies = current_user.movies.limit(2).order("RANDOM()")
+    if rand(1..10) > 5 && current_user.movies.count > 50
+      @movies = [current_user.rankings.sort_by(&:win_percentage).map {|ranking| ranking.movie }.pop(10).sample()]
+      @movies.push(Movie.order("RANDOM()").first).to_json
     else
       @movies = Movie.limit(2).order("RANDOM()")
     end
