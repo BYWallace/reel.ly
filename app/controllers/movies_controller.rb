6cabc228097c127c0cb4f5d281b3a15fd6020739
BYWallace <bwallace90@gmail.com>
2014-07-15 16:01:28 -0400

    more randomization tweaking

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,9 +1,12 @@
 class MoviesController < ApplicationController
   def index
+    random_number = rand(1..10)
     # postgreSQL
-    if rand(1..10) > 3 && current_user.movies.count > 2
+    if random_number < 6 && current_user.movies.count > 2
       @movies = [current_user.rankings.where(has_watched: true).sort_by(&:win_percentage).map {|ranking| ranking.movie }.pop(10).sample()]
       @movies.push(Movie.order("RANDOM()").first).to_json
+    elsif random_number == 6
+      @movies = current_user.rankings.where(has_watched: true).sort_by(&:win_percentage).map {|ranking| ranking.movie }.pop(10).sample(2).to_json
     else
       @movies = Movie.limit(2).order("RANDOM()")
     end
