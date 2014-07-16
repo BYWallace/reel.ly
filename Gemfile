701895eac757c0be5180ad47d0a702962db4cfb5
BYWallace <bwallace90@gmail.com>
2014-07-15 21:41:00 -0400

    new source for poster images since imdb rejected me :(

--- Gemfile
+++ Gemfile
@@ -29,6 +29,7 @@ gem 'devise'
 gem 'imdb', '~> 0.8'
 gem 'pry-rails', group: :development
 gem 'rails_12factor', group: :production
+gem 'themoviedb'
 
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -8,10 +8,17 @@ class Movie < ActiveRecord::Base
       Movie.create(
         title: movie_title,
         year: movie.year,
-        poster_url: movie.poster,
         rating: movie.rating,
-        imdb_url: movie.url
+        imdb_url: movie.id
       )
+    end
+  end
+
+  def self.update_posters
+    Tmdb::Api.key("cb3bad947e989cd047e54de3cdbe0ce3")
+    Movie.all.each do |movie|
+      poster_path = Tmdb::Find.imdb_id("tt#{movie.imdb_url}")["movie_results"][0]["poster_path"]
+      movie.update(poster_url: poster_path)
     end
   end
 end
