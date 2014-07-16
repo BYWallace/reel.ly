4eb53fbb61441c886f921ed11eddc86deda6946c
BYWallace <bwallace90@gmail.com>
2014-07-15 23:45:04 -0400

    fixing comparison generation for new users

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -2,7 +2,7 @@ class MoviesController < ApplicationController
   def index
     random_number = rand(1..10)
     # postgreSQL
-    if random_number < 6 && current_user.movies.count > 2
+    if random_number < 6 && current_user.movies.count > 10
       @movies = [current_user.rankings.where(has_watched: true).sort_by(&:win_percentage).map {|ranking| ranking.movie }.pop(10).sample()]
       @movies.push(Movie.order("RANDOM()").first).to_json
     elsif random_number == 6
